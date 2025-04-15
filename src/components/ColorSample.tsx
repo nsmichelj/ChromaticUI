102515de1a2d4ed38a03ab75418052f42b4061dc
NSMichelJ <nunezsalmeronm@gmail.com>
2025-04-15 18:00:44 -0400

    fix(ColorSample): set default background color to black when color is not provided

--- src/components/ColorSample.tsx
+++ src/components/ColorSample.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export default function ColorSample({color, selected, onClick, children}: {color: string, selected?:boolean, onClick: () => void, children?: React.ReactNode}) {
   return (
     <div
       className={`${selected ? 'size-10' : 'size-8' } rounded-lg cursor-pointer transition-transform hover:scale-105`}
       style={{
-        background: color,
-        outline: `${color} solid 2px`,
+        background: color || "#000000",
+        outline: `${color || "#000000"} solid 2px`,
         outlineOffset: "2px",
       }}
       onClick={onClick}
